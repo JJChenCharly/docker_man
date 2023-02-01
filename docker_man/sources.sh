66e1c466712d61369102b7f336b5df03c39c656d
JingJieChen_Charly <jingjie.chencharly@gmail.com>
2023-02-02 00:55:52 +0800

    easy find

---
+++ docker_man/sources.sh
@@ -0,0 +1,78 @@
+# ubuntu only
+# 清华 ----
+# https://mirrors.tuna.tsinghua.edu.cn/
+
+# 阿里 ----
+# https://developer.aliyun.com/mirror/
+# 14.04.5 LTS trusty ----
+###################
+deb https://mirrors.aliyun.com/ubuntu/ trusty main restricted universe multiverse
+deb-src https://mirrors.aliyun.com/ubuntu/ trusty main restricted universe multiverse
+deb https://mirrors.aliyun.com/ubuntu/ trusty-security main restricted universe multiverse
+deb-src https://mirrors.aliyun.com/ubuntu/ trusty-security main restricted universe multiverse
+
+deb https://mirrors.aliyun.com/ubuntu/ trusty-updates main restricted universe multiverse
+deb-src https://mirrors.aliyun.com/ubuntu/ trusty-updates main restricted universe multiverse
+
+deb https://mirrors.aliyun.com/ubuntu/ trusty-backports main restricted universe multiverse
+deb-src https://mirrors.aliyun.com/ubuntu/ trusty-backports main restricted universe multiverse
+
+## Not recommended
+# deb https://mirrors.aliyun.com/ubuntu/ trusty-proposed main restricted universe multiverse
+# deb-src https://mirrors.aliyun.com/ubuntu/ trusty-proposed main restricted universe multiverse
+###################
+
+# 16.04 xenial ----
+###################
+deb https://mirrors.aliyun.com/ubuntu/ xenial main
+deb-src https://mirrors.aliyun.com/ubuntu/ xenial main
+
+deb https://mirrors.aliyun.com/ubuntu/ xenial-updates main
+deb-src https://mirrors.aliyun.com/ubuntu/ xenial-updates main
+
+deb https://mirrors.aliyun.com/ubuntu/ xenial universe
+deb-src https://mirrors.aliyun.com/ubuntu/ xenial universe
+deb https://mirrors.aliyun.com/ubuntu/ xenial-updates universe
+deb-src https://mirrors.aliyun.com/ubuntu/ xenial-updates universe
+
+deb https://mirrors.aliyun.com/ubuntu/ xenial-security main
+deb-src https://mirrors.aliyun.com/ubuntu/ xenial-security main
+deb https://mirrors.aliyun.com/ubuntu/ xenial-security universe
+deb-src https://mirrors.aliyun.com/ubuntu/ xenial-security universe
+###################
+
+# ubuntu 18.04 bionic ----
+###################
+deb https://mirrors.aliyun.com/ubuntu/ bionic main restricted universe multiverse
+deb-src https://mirrors.aliyun.com/ubuntu/ bionic main restricted universe multiverse
+
+deb https://mirrors.aliyun.com/ubuntu/ bionic-security main restricted universe multiverse
+deb-src https://mirrors.aliyun.com/ubuntu/ bionic-security main restricted universe multiverse
+
+deb https://mirrors.aliyun.com/ubuntu/ bionic-updates main restricted universe multiverse
+deb-src https://mirrors.aliyun.com/ubuntu/ bionic-updates main restricted universe multiverse
+
+# deb https://mirrors.aliyun.com/ubuntu/ bionic-proposed main restricted universe multiverse
+# deb-src https://mirrors.aliyun.com/ubuntu/ bionic-proposed main restricted universe multiverse
+
+deb https://mirrors.aliyun.com/ubuntu/ bionic-backports main restricted universe multiverse
+deb-src https://mirrors.aliyun.com/ubuntu/ bionic-backports main restricted universe multiverse
+###################
+
+# ubuntu 20.04 focal ----
+###################
+deb https://mirrors.aliyun.com/ubuntu/ focal main restricted universe multiverse
+deb-src https://mirrors.aliyun.com/ubuntu/ focal main restricted universe multiverse
+
+deb https://mirrors.aliyun.com/ubuntu/ focal-security main restricted universe multiverse
+deb-src https://mirrors.aliyun.com/ubuntu/ focal-security main restricted universe multiverse
+
+deb https://mirrors.aliyun.com/ubuntu/ focal-updates main restricted universe multiverse
+deb-src https://mirrors.aliyun.com/ubuntu/ focal-updates main restricted universe multiverse
+
+# deb https://mirrors.aliyun.com/ubuntu/ focal-proposed main restricted universe multiverse
+# deb-src https://mirrors.aliyun.com/ubuntu/ focal-proposed main restricted universe multiverse
+
+deb https://mirrors.aliyun.com/ubuntu/ focal-backports main restricted universe multiverse
+deb-src https://mirrors.aliyun.com/ubuntu/ focal-backports main restricted universe multiverse
+###################
