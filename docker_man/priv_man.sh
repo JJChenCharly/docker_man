2e9b1402a9a138f4b9865ccedf9ba1742c264506
HeJunWeijoy <125254157+HeJunWeijoy@users.noreply.github.com>
2023-02-26 19:48:24 +0800

    Update priv_man.sh

--- docker_man/priv_man.sh
+++ docker_man/priv_man.sh
@@ -37,6 +37,8 @@ deb-src https://mirrors.aliyun.com/ubuntu/ bionic-backports main restricted univ
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 3B4FE6ACC0B21F32
 
 apt update
+apt upgrade
+
 apt install vim
 apt-get install screen
 
