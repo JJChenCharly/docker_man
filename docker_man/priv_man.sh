003f3496c51bf9fc70c88417776b47eb6193ad9d
JingJieChen_Charly <jingjie.chencharly@gmail.com>
2023-03-04 12:38:01 +0800

    Update priv_man.sh

--- docker_man/priv_man.sh
+++ docker_man/priv_man.sh
@@ -5,20 +5,26 @@ docker pull quay.io/qiime2/core:2022.11
 # run image with certain setup ----
 
 docker run -it -m 60G --gpus=all --name NAAM \
--v D:/docker_share_D:/data/docker_qiime2_share_container_D \
+-v D:/docker_share_D:/data/docker_qiime2_share_container_D \ # setup mount and shared folder
 -v E:/docker_qiime2_E：/data/docker_qiime2_share_container_E \
 -v F:/docker_share_F:/data/docker_qiime2_share_container_F \
 -p 80:80 pytorch/pytorch:1.12.1-cuda11.3-cudnn8-runtime /bin/bash
 
-# source bionic OPTIONAL ----
+# source bionic OPTIONAL!!!!!!!!! ----
 # https://developer.aliyun.com/mirror/
 
-cp /etc/apt/sources.list /etc/apt/sources.list.bak
+# back up source list
+cp /etc/apt/sources.list /etc/apt/sources.list.bak 
 
+# clean current source list
 sed -i 's/.*//' /etc/apt/sources.list
+# or
+echo "" > /etc/apt/sources.list
 
+# some containers may need this
 apt-get install apt-transport-https
 
+# write in new sources
 echo "deb https://mirrors.aliyun.com/ubuntu/ bionic main restricted universe multiverse
 deb-src https://mirrors.aliyun.com/ubuntu/ bionic main restricted universe multiverse
 
@@ -34,17 +40,19 @@ deb-src https://mirrors.aliyun.com/ubuntu/ bionic-updates main restricted univer
 deb https://mirrors.aliyun.com/ubuntu/ bionic-backports main restricted universe multiverse
 deb-src https://mirrors.aliyun.com/ubuntu/ bionic-backports main restricted universe multiverse" | tee -a /etc/apt/sources.list
 
+# some source may need new key
 apt-get install gnupg
 apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 3B4FE6ACC0B21F32
 
+# some normal set up
 apt update
 apt upgrade
 
 apt install vim
 apt-get install screen
 
-# setup OPTIONAL----
-
+# setup for jupyter ----
+# you may not need every thing if you already have them
 apt install python3
 apt-get install python3-pip
 
@@ -54,25 +62,30 @@ jupyter contrib nbextension install --user
 
 pip install ipython -i https://pypi.tuna.tsinghua.edu.cn/simple
 
+# input ipython in prompt and start ipython interface
 $ ipython
 
     from notebook.auth import passwd
 
     passwd()
-
+       
+    # after input your own password and confirm, system will pring this copy and keep it somewhere
     'argon2:$argon2id$v=19$m=10240,t=10,p=8$2ENvg6zjYaYxuOnHqxeShg$lBmZl8JTjMkegE2wPjA8aOv+m3PvJdhL634Fxwyp2QE'
 
     exit()
 
 # jupy set ----
+# create .config
 jupyter notebook --generate-config --allow-root
 
+# edit with one of this
 vi ~/.jupyter/jupyter_notebook_config.py
-
 vim ~/.jupyter/jupyter_notebook_config.py
-# into ~/.jupyter/jupyter_notebook_config.py ----
+
+# write these into ~/.jupyter/jupyter_notebook_config.py ----
 c.NotebookApp.ip = '*'
 
+# replace entire string after u with the string you get after input password in ipython step
 c.NotebookApp.password =u'argon2:$argon2id$v=19$m=10240,t=10,p=8$2ENvg6zjYaYxuOnHqxeShg$lBmZl8JTjMkegE2wPjA8aOv+m3PvJdhL634Fxwyp2QE'
 
 c.NotebookApp.open_browser = False
@@ -82,4 +95,4 @@ c.NotebookApp.port = 80
 # exec ----
 docker exec -it c4311f036c31 /bin/bash
 
-# open in browser
+# `jupyter notebook --allow-root` and then open in browser
